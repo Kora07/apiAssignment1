36569b8d61176fc124f61c3fa5a05409acb7cb8a
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-24 11:56:43 +0530

    Environment variables code added

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const port = 3010;
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+require('dotenv').config();
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+console.log(config)
+
+app.get('/', (request, response) => {
+  response.send(config);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Running at http://localhost:${port}`);
 });
+
+
+module.exports = config;
